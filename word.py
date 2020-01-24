25ffb1b15ac9e1217b7e7c27a69eabf7eb0dc292
sturang <60178668+sturang@users.noreply.github.com>
2020-01-24 17:32:49 +0530

    Add files via upload

---
+++ word.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+# pixel position by
+pyautogui.position()
+
+#click on the firefox button
+pyautogui.click(28,160)
+
+pyautogui.typewrite("Hello")
+
+pyautogui.typewrite(["Enter"])
+
